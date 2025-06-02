8968a09701871b8910c834a3f30b5e83e8b6af49
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-06-02 18:28:13 +0500

    changed

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,11 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
+  app.enableCors({
+    origin: 'https://semoorg-front.vercel.app',
+    methods: ['POST'],
+  });
   
   await app.listen(process.env.PORT ?? 3000);
 }
